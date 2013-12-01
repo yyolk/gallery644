9f28ec1d39ac87c68320c93cd60674d85ba27d3d
Joseph Yølk Chiocchi <joe@yolk.cc>
2013-12-01 17:12:37 -0500

    colorz

--- public/js/main.js
+++ public/js/main.js
@@ -3,5 +3,12 @@ require([
 ],
 function ( bootstrap, $ ) {
     'use strict';
+    function rc(){
+      return Math.floor(Math.random()*255);
+    }
     //start();
+    setInterval(function(){
+      $('h1').css('color', 'rgb('+rc()+','+rc()+','+rc()+')');
+      $('body').css('background-color', 'rgb('+rc()+','+rc()+','+rc()+')');
+    }, 500);
 });
